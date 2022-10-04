8f384ce696dced9635c3d21a49253b8f1f27450e
Choisuren <choisurena@gmail.com>
2022-10-04 19:09:41 +0800

    version 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // шоо хаях тоглоомонд шаардлагатай хувьсагчдыг зарлая
 
-var activePlayer = 1;
+var activePlayer = 0;
 var scores = [0, 0];
 var roundScore = 0;
 var dice = Math.floor(Math.random(1, 6) * 6) + 1;
@@ -11,10 +11,26 @@ console.log(dice);
 console.log((document.getElementById("score-0").textContent = dice));
 console.log((document.getElementById("score-1").textContent = dice));
 console.log(document.querySelector(".dice"));
-console.log(
-  document.querySelector(".btn-roll").addEventListener("click", shooShideh)
-);
 
-function shooShideh() {
-  console.log("shoo buuulaa");
-}
+// Шоог шидэх эвент листенер
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random(1, 6) * 6) + 1;
+  document.querySelector(".dice").src = "dice-" + diceNumber + ".png";
+  console.log("shoo buuulaa " + diceNumber);
+
+  if (diceNumber != 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+    console.log(roundScore);
+  } else {
+    // if (activePlayer == 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer == 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.add("active");
+  }
+});
